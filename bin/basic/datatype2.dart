b1cdd7bf4d3d59272afab3a63b5aa7909cd7e7f8
am607 <akbarcp607@gmail.com>
2022-04-12 12:53:35 +0530

    hello_world

--- bin/basic/datatype2.dart
+++
@@ -1,5 +0,0 @@
-void main(List<String> args) {
-  // differnce bitween dynamic and var
-  // dynamic can rewrite  with differnt datatype but var can rewrite with hte same type
-  // final datatype also like var and not rewritable
-}
